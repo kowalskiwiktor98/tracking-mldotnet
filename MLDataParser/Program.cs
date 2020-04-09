33ca3c5c47f4b30d5e9ca6fb3c4ece428e5c4ffd
Wiktor Kowalski <wiktor9196667@gmail.com>
2020-04-09 09:42:44 +0200

    init

---
+++ MLDataParser/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MLDataParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //heders
+            string headers = Console.ReadLine();
+            Console.ReadLine();
+            Console.WriteLine(headers);
+            string input = "";
+            int lineCounter = 0;
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            do
+            {
+                input = Console.ReadLine();
+                //lineCounter++;
+                //if (lineCounter % 1000000 == 0) Console.WriteLine(lineCounter+" lines done");
+                if (input == "") continue;
+                #region substring
+                var code = input.Substring(0, 41).Trim()+';';
+                var createDate = input.Substring(41, 24).Trim() + ';';
+                var firstDate = input.Substring(65, 24).Trim() + ';';
+                var lastDate = input.Substring(89, 24).Trim() + ';';
+                var receiverZip= input.Substring(113, 33).Trim() + ';';
+                var receiverCountry= input.Substring(146, 20).Trim() + ';';
+                var senderZip = input.Substring(168, 33).Trim() + ';';
+                var senderCountry = input.Substring(201, 20).Trim() + ';';
+                var weight = input.Substring(221, 10).Trim() + ';';
+                var contract = input.Substring(261, 24).Trim() + ';';
+                var xlid = input.Substring(322).Trim();
+                #endregion
+
+                Console.WriteLine(code+createDate+firstDate+lastDate+receiverZip+receiverCountry+senderZip+senderCountry+weight+contract+xlid);
+            } while (input != null && lineCounter < 10);
+
+            timer.Stop();
+            Console.WriteLine("Time elapsed " + timer.Elapsed);
+
+            //Console.WriteLine("Done");
+            //Console.Read();
+        }
+    }
+}
---
+++ MLTrainDataGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MLTrainDataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("begin;zipFrom;zipTo;duration");
+            Console.ReadLine();
+            string input = "";
+            int lineCounter = 0;
+            do
+            {
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) continue;
+                if (input.Contains("NULL")) continue;
+                lineCounter++;
+                //if (lineCounter % 1000000 == 0) Console.WriteLine(lineCounter+" lines done");
+                var fields = input.Split(';');
+                var begin = DateTime.Parse(fields[2]);
+                var zipFrom = fields[6];
+                var zipTo = fields[4];
+                var duration = (float)DateTime.Parse(fields[3]).Subtract(begin).TotalHours;
+                if (duration < 1) continue;
+                Console.WriteLine(begin + ";" + zipFrom.Replace(' ', '-') + ";" + zipTo.Replace(' ', '-') + ";" + duration.ToString().Replace(',', '.'));
+
+
+            } while (input != null && lineCounter < 2_000_000);
+        }
+    }
+}
